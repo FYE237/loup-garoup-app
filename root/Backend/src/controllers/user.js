a77efb52ce5d755b317bbffc092a4c8dfd7ddaea
FEZEU YOUMBI Emmanuel <yfezeu.emma@gmail.com>
2023-04-15 13:12:37 +0200

    TOPIC : Endpoint pour vérifier si un pseudo est déjà utilisé

--- root/Backend/src/controllers/user.js
+++ root/Backend/src/controllers/user.js
@@ -50,6 +50,30 @@ module.exports = {
         res.json({status: true, message: 'Returning user', data});
     },
 
+    /**
+     * On vérifie si un username est 
+     * @param {*} req 
+     * @param {*} res 
+     */
+    async checkDuplicateUserById(req, res){
+        // #swagger.tags = ['Users']
+        // #swagger.summary = 'Check if the pseudo that the user wants to create is already used'
+        // #swagger.parameters['id'] = { in: 'path', description:'id:pseudo of the user to find'}
+    
+            if(!has(req.params, 'id'))
+                throw new CodeError('You must specify the name ', status.BAD_REQUEST)
+            
+            let id = req.params.id;
+    
+            //On specifie les champs qu'on ne souhaite pas récupérer par le findOne
+            let data =  await User.findOne({name:id}).select({email:1})
+     
+            if(data)
+                throw new CodeError('Duplicate user', status.NOT_FOUND)
+    
+            res.json({status: true, message: 'No User found'});
+        },
+
 
     /**
      * On récupere tous les utilisateurs
@@ -86,7 +110,8 @@ module.exports = {
         
         user.save()
         .then(() => res.json({status: true, message: 'User Added'}))
-        .catch(() => {throw  new CodeError('Add failed', status.BAD_REQUEST)})
+        //.catch(() => {throw  new CodeError('Add failed', status.BAD_REQUEST)})
+        .catch(() => res.json({status: false, message: 'Add failed'}))
 
     },
 
