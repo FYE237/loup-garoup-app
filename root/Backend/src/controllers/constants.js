c857f73ec819569e23b2a0c4119b5a32ffc61759
SamuelVanie <samuelmichaelvanie@gmail.com>
2023-04-23 01:08:07 +0200

    feat: create the constants for the special powers of the game

--- root/Backend/src/controllers/constants.js
+++ root/Backend/src/controllers/constants.js
@@ -3,36 +3,37 @@
  *  part of the game
  */
 const GAME_STATUS = {
-  enAttente : "enAttente",
-  jour : "jour",
-  soir : "soir",
-  finJeu : "finJeu"
-}
+  enAttente: "enAttente",
+  jour: "jour",
+  soir: "soir",
+  finJeu: "finJeu",
+};
 
 const PLAYER_STATUS = {
-  vivant : "vivant",
-  mort : "mort"
-}
+  vivant: "vivant",
+  mort: "mort",
+};
 
 const ROLE = {
-  villageois : "villageois",
-  loupGarrou : "loupGarrou",
-  //this value we will be used when the game is on hold and 
-  //it has not started yet but when the game starts 
+  villageois: "villageois",
+  loupGarou: "loupGarou",
+  speciauxLoup: ["voyanteLoup", "spiritismeLoup", "contamination"],
+  speciauxHumain: ["voyanteHumain", "spiritismeHumain", "insomnie"],
+  //this value we will be used when the game is on hold and
+  //it has not started yet but when the game starts
   //every player will be associated a certain role
-  noRole : "norole"
-}
+  noRole: "norole",
+};
 
 const CHAT_TYPE = {
-  general : "general_chat",
-  loup : "loup_chat",
-  customChat : "customChat"
-}
+  general: "general_chat",
+  loup: "loup_chat",
+  customChat: "customChat",
+};
 
-
-module.exports =  {
+module.exports = {
   GAME_STATUS,
   PLAYER_STATUS,
   CHAT_TYPE,
-  ROLE
-}
+  ROLE,
+};
