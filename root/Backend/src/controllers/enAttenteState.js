c0b8a15d878612bb4daf0d646ce3ed5ff34c0fb1
SamuelVanie <samuelmichaelvanie@gmail.com>
2023-04-26 16:34:50 +0200

    fix: change the "proportion_loup" to "proportionLoup"
    Change the "proportion_loup" to "proportionLoup" so that the
    wolveNumber get correctly calculated

--- root/Backend/src/controllers/enAttenteState.js
+++ root/Backend/src/controllers/enAttenteState.js
@@ -1,13 +1,14 @@
 // const Joueur_partie_role = require('../models/joueur_partie_role')
 
-const GameState = require('./gameState');
-const {GAME_STATUS, CHAT_TYPE, ROLE, GAME_VALUES} = require("./constants")
-const Joueur_partie_role = require('../models/joueur_partie_role')
-const {Chat} = require('../models/chat')
-const { v4: uuidv4 } = require('uuid');
+const GameState = require("./gameState");
+const { GAME_STATUS, CHAT_TYPE, ROLE, GAME_VALUES } = require("./constants");
+const Joueur_partie_role = require("../models/joueur_partie_role");
+const { Chat } = require("../models/chat");
+const { v4: uuidv4 } = require("uuid");
 
-const debug = require('debug')('EnAttenteState');
+const { SPECIAL_POWERS } = require("./constants");
 
+const debug = require("debug")("EnAttenteState");
 
 class EnAttenteState extends GameState {
   constructor(context) {
@@ -21,54 +22,68 @@ class EnAttenteState extends GameState {
    */
   async handleRejoindreJeu(nsp, socket, pseudo, socket_id) {
     //We save this so that we can use it later
-    //the nsp reference needs to get updated everytime a player joins 
-    //so that the rooms and sockets are well configured, if we take a reference to 
+    //the nsp reference needs to get updated everytime a player joins
+    //so that the rooms and sockets are well configured, if we take a reference to
     //the same object this will not be necessary but we have nothing
     //to lose with this approach
     this.context.nsp = nsp;
     debug("[EnAttenteState] handleRejoindreJeu");
-    const player_id = await this.getPlayerId(pseudo) 
-    debug("[EnAttenteState] Joining player pseudo = "+pseudo+" + player_id = "+ player_id._id)
-    debug("[EnAttenteState] Game id " + this.context.partieId)
-    if (!player_id){
+    const player_id = await this.getPlayerId(pseudo);
+    debug(
+      "[EnAttenteState] Joining player pseudo = " +
+        pseudo +
+        " + player_id = " +
+        player_id._id
+    );
+    debug("[EnAttenteState] Game id " + this.context.partieId);
+    if (!player_id) {
       debug("[EnAttenteState]Player is unknown");
     }
     const res = await Joueur_partie_role.updateOne(
-          {id_joueur:player_id._id, id_partie : this.context.partieId},
-          {socket_id : socket_id}).then(result => {return result;})
+      { id_joueur: player_id._id, id_partie: this.context.partieId },
+      { socket_id: socket_id }
+    ).then((result) => {
+      return result;
+    });
     //We check to see if a match was found
     if (res.n == 0) {
       //no match, we leave immediatly
       //as the player is not registerd in the game
-      debug("Player is not registerd in the game")
+      debug("Player is not registerd in the game");
       return;
     }
-    if (!this.addPlayerIntoContext(pseudo)){
+    if (!this.addPlayerIntoContext(pseudo)) {
       //Player is in the game already
-      debug("Player is already in the game "+pseudo);
-      debug("Player table "+this.context.pseudoList);
+      debug("Player is already in the game " + pseudo);
+      debug("Player table " + this.context.pseudoList);
       return;
     }
     //We connect the player to the game room  one we have verified
     //his id and made sure is registered to the game
     socket.join(this.context.roomId);
     let data = {
-      message : pseudo + " has joined the game",
-      status : GAME_STATUS.enAttente,
-      nb_players_actuel : this.context.nb_actif_players,
-      nb_participant_souhaite : this.context.nbParticipantSouhaite,
-      temps_restant : this.remainingTime(),
+      message: pseudo + " has joined the game",
+      status: GAME_STATUS.enAttente,
+      nb_players_actuel: this.context.nb_actif_players,
+      nb_participant_souhaite: this.context.nbParticipantSouhaite,
+      temps_restant: this.remainingTime(),
       // room :  chat_id  //TODO delete this since it is used for debugging
-    }
-    debug("[EnAttenteState] PLayer "+pseudo+" has joined the game : " + this.context.partieId);
-    nsp.to(this.context.roomId).emit('status-game', data);
-    if (this.context.nb_actif_players == this.context.nbParticipantSouhaite){
-      if (this.timeout){
+    };
+    debug(
+      "[EnAttenteState] PLayer " +
+        pseudo +
+        " has joined the game : " +
+        this.context.partieId
+    );
+    nsp.to(this.context.roomId).emit("status-game", data);
+    if (this.context.nb_actif_players == this.context.nbParticipantSouhaite) {
+      if (this.timeout) {
         clearTimeout(this.timeout);
         this.launchGame();
-      }
-      else{
-        throw new Error("Timer was not set up correctly or  we are not in the correct state");
+      } else {
+        throw new Error(
+          "Timer was not set up correctly or  we are not in the correct state"
+        );
       }
     }
     await this.getCountRole(ROLE.noRole);
@@ -79,7 +94,7 @@ class EnAttenteState extends GameState {
     if (!this.context.pseudoList.includes(pseudo)) {
       this.context.nb_actif_players++;
       this.context.nbAlivePlayer++;
-      this.context.pseudoList.push(pseudo)
+      this.context.pseudoList.push(pseudo);
       return true;
     }
     return false;
@@ -107,77 +122,84 @@ class EnAttenteState extends GameState {
       id_joueur: id_joueur,
     });
     let data = {
-      message : name + " has left the game",
-      status : GAME_STATUS.enAttente,
-      nb_players_actuel : this.context.nb_actif_players,
-      nb_participant_souhaite : this.context.nbParticipantSouhaite,
-      temps_restant : this.remainingTime(),
+      message: name + " has left the game",
+      status: GAME_STATUS.enAttente,
+      nb_players_actuel: this.context.nb_actif_players,
+      nb_participant_souhaite: this.context.nbParticipantSouhaite,
+      temps_restant: this.remainingTime(),
     };
     nsp.to(this.context.roomId).emit("status-game", data);
   }
 
-
-
   /**
-   * Creates the general chat and loup chat 
+   * Creates the general chat and loup chat
    * and adds players to their respectif rooms
    */
-  async createGamechat(){
-    debug("Creating chats rooms for the game : "+ this.context.partieId);
-    if (this.context.nsp == null){
-      debug("No player joined the game thus this game context is connected to any socket")
+  async createGamechat() {
+    debug("Creating chats rooms for the game : " + this.context.partieId);
+    if (this.context.nsp == null) {
+      debug(
+        "No player joined the game thus this game context is connected to any socket"
+      );
       return;
     }
 
     const generalChatRoom = uuidv4();
-    this.context.generalChatRoom = generalChatRoom; 
+    this.context.generalChatRoom = generalChatRoom;
     const chatGeneral = new Chat({
       chat_type: CHAT_TYPE.general_chat,
-      id_partie: this.context.partieId, 
-      chat_room_id : generalChatRoom
-    })
+      id_partie: this.context.partieId,
+      chat_room_id: generalChatRoom,
+    });
 
-    await chatGeneral.save()
-    .catch((err) => {debug("General chat wasn't created  error"+err)})
+    await chatGeneral.save().catch((err) => {
+      debug("General chat wasn't created  error" + err);
+    });
 
     const loupChatRoom = uuidv4();
-    this.context.loupChatRoom = loupChatRoom; 
+    this.context.loupChatRoom = loupChatRoom;
     const chatLoup = new Chat({
       chat_type: CHAT_TYPE.loup_chat,
-      id_partie: this.context.partieId, 
-      chat_room_id : loupChatRoom
-    })
-    await chatLoup.save()
-    .catch((err) => {debug("Chat loup chat wasn't created  error"+err)})
+      id_partie: this.context.partieId,
+      chat_room_id: loupChatRoom,
+    });
+    await chatLoup.save().catch((err) => {
+      debug("Chat loup chat wasn't created  error" + err);
+    });
 
-    const playerJoueurLink = await Joueur_partie_role.find({ id_partie: this.context.partieId });
-    if (!playerJoueurLink){
-      debug("Players were not found in the database")
+    const playerJoueurLink = await Joueur_partie_role.find({
+      id_partie: this.context.partieId,
+    });
+    if (!playerJoueurLink) {
+      debug("Players were not found in the database");
     }
-    playerJoueurLink.map((player) =>{
+    playerJoueurLink.map((player) => {
       let playerSocket = this.context.nsp.sockets.get(player.socket_id);
-      if (!playerSocket){
-        debug("Player socket was not found, id of the player = " + player.id_joueur)
+      if (!playerSocket) {
+        debug(
+          "Player socket was not found, id of the player = " + player.id_joueur
+        );
         return;
       }
-      playerSocket.join(generalChatRoom)
-      if (player.role === ROLE.loupGarrou){
-        playerSocket.join(loupChatRoom)
+      playerSocket.join(generalChatRoom);
+      if (player.role === ROLE.loupGarrou) {
+        playerSocket.join(loupChatRoom);
       }
-      if (player.statut === SPECIAL_POWERS.insomnie){
-        playerSocket.join(loupChatRoom)
-      }
-      else if (player.role === ROLE.noRole){
+      if (player.statut === SPECIAL_POWERS.insomnie) {
+        playerSocket.join(loupChatRoom);
+      } else if (player.role === ROLE.noRole) {
         //TODO REMOVE THE COMMENT FROM HERE WHEN WE PASS TO PRODUCTION
         // throw new Error("A player in the game does not have a role")
       }
-    })
-    debug("Chats rooms have been created for the game  : "+ this.context.partieId);
-    debug("general chat room id  = " + generalChatRoom)
-    debug("loup chat room id  = " + loupChatRoom)
+    });
+    debug(
+      "Chats rooms have been created for the game  : " + this.context.partieId
+    );
+    debug("general chat room id  = " + generalChatRoom);
+    debug("loup chat room id  = " + loupChatRoom);
     /**
      * TODO : ADD A FUNCTION THAT ADD THE PLAYERS WITH THE INSOMIE POWER TO THE LOUP GARROU
-     * ROOM BUT ADD RESTRICTIONS SO THEY ARE NOT ABLE TO SEND MESSAGES AND CANNOT VIEW THE NAMES OF 
+     * ROOM BUT ADD RESTRICTIONS SO THEY ARE NOT ABLE TO SEND MESSAGES AND CANNOT VIEW THE NAMES OF
      * THE PEOPLE THAT SEND THE MESSAGES IN THE CHAT ROOM
      */
     // return generalChatRoom; TODO: REMOVE THIS ;; Used for debugging
@@ -189,17 +211,17 @@ class EnAttenteState extends GameState {
    * @returns 0 if it failed and 1 it it was successfull
    */
   async endCode() {
-    if (!this.context.nsp){
+    if (!this.context.nsp) {
       debug("No one connected to the game");
       return 0;
     }
     if (this.context.nb_actif_players < GAME_VALUES.min_players) {
-      debug("Game cannot get started that are very litle players")
+      debug("Game cannot get started that are very litle players");
       return 0;
     }
     // computing of the number of wolves in the game
     const wolvesNumber = Math.ceil(
-      this.context.nb_actif_players * this.context.proportion_loup
+      this.context.nb_actif_players * this.context.proportionLoup
     );
 
     // checking if we should have special powers depending on the probability entered by the user
@@ -271,7 +293,7 @@ class EnAttenteState extends GameState {
     for (const wolves of assignedWolvesRoles) {
       playerRolesList[wolves.indice].role = ROLE.loupGarou;
       playerRolesList[wolves.indice].pouvoir_speciaux = wolves.role;
-      playerRolesList[wolves.indice].save()
+      playerRolesList[wolves.indice].save();
       //   (err) => {
       //   if (err) {
       //     console.log(err);
@@ -283,12 +305,13 @@ class EnAttenteState extends GameState {
       playerRolesList[human.indice].role = ROLE.villageois;
       playerRolesList[human.indice].pouvoir_speciaux = human.role;
       playerRolesList[human.indice].save();
-        // (err) => {
-        // if (err) {
-        //   console.log(err);
-        // }
+      // (err) => {
+      // if (err) {
+      //   console.log(err);
+      // }
       // });
     }
+
     this.createGamechat();
     return 1;
   }
@@ -305,22 +328,22 @@ class EnAttenteState extends GameState {
     if (this.context.state != this.context.stateEnAttente) {
       throw new Error("Game context is not in the en attente state");
     }
-    if (this.nb_actif_players == 0){
-      debug("No one is in the game ")
+    if (this.nb_actif_players == 0) {
+      debug("No one is in the game ");
       return;
     }
     //This will change the state and before it does that it will
-    //call the endcode method of the currect state class 
+    //call the endcode method of the currect state class
     //and the setup method of the next state class
     debug("Starting the game with the day state");
-    if (!this.context.setState(this.context.stateJour)){
+    if (!this.context.setState(this.context.stateJour)) {
       this.context.setState(this.context.stateNuit);
     }
   }
 
   remainingTime() {
     const elapsedTime = Date.now() - this.startTime;
-    const timeLeft = this.context.tempsDebut*1000 - elapsedTime;
+    const timeLeft = this.context.tempsDebut * 1000 - elapsedTime;
     return timeLeft;
   }
   /**
@@ -330,8 +353,14 @@ class EnAttenteState extends GameState {
   async setupCode() {
     await this.context.initAttributs();
     this.startTime = Date.now();
-    debug("Launched game timer, starting the game in :", this.context.tempsDebut*1000+ " ms");
-    this.timeout = setTimeout(this.startGameTimer.bind(this), this.context.tempsDebut*1000);
+    debug(
+      "Launched game timer, starting the game in :",
+      this.context.tempsDebut * 1000 + " ms"
+    );
+    this.timeout = setTimeout(
+      this.startGameTimer.bind(this),
+      this.context.tempsDebut * 1000
+    );
   }
 }
 
