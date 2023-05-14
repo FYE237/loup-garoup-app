b52c8d2a81cebf76317d50c7af14584680f6dd62
jiaxuanYang <jiaxuan.yang@grenoble-inp.org>
2023-05-14 15:02:34 +0200

    update

--- root/frontend/loup-garou_front/constants/links.js
+++ root/frontend/loup-garou_front/constants/links.js
@@ -1,6 +1,6 @@
 const LINKS = {
     // backend : "https://loup-garoup-app.onrender.com"
-    backend : "http://192.168.0.14:3000"
+    backend : "http://192.168.1.40:3000"
     // backend : "https://projetwebimag.osc-fr1.scalingo.io"
 }
 
