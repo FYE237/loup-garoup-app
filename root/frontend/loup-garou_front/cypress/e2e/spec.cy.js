ee57cfd53ad8f989fa16be67d4302a54b8e48163
jiaxuanYang <jiaxuan.yang@grenoble-inp.org>
2023-05-14 17:33:58 +0200

    on arrive a pousser le bouton login

--- root/frontend/loup-garou_front/cypress/e2e/spec.cy.js
+++ root/frontend/loup-garou_front/cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:19000/WelcomePage')
+    cy.get('input').first().type('sam')
+    cy.get('input').last().type('123456')
+    // cy.get('[title="Login"]').click()
+    cy.get('[data-testid="loginbutton"]').click();
   })
 })
