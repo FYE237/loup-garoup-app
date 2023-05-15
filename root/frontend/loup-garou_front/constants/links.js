047e6e40ce36e8d47e20a33b1daf389f52175099
Frikha Mehdi <Mehdi.Frikha@grenoble-inp.org>
2023-05-15 18:19:44 +0200

    added latest render link

--- root/frontend/loup-garou_front/constants/links.js
+++ root/frontend/loup-garou_front/constants/links.js
@@ -1,5 +1,5 @@
 const LINKS = {
-  backend : "https://loup-garoup-app.onrender.com"
+  backend : "https://projet-web-g3.onrender.com"
   // backend: 'http://10.188.228.135:3000'
   // backend: 'http://192.168.0.14:3000'
   // backend: 'http://192.168.1.40:3000'
