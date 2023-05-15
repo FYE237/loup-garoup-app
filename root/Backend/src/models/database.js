3704c72373505be7b4017666371b81de2f03119e
Frikha Mehdi <Mehdi.Frikha@grenoble-inp.org>
2023-05-15 18:01:54 +0200

    checking backend database

--- root/Backend/src/models/database.js
+++ root/Backend/src/models/database.js
@@ -18,7 +18,7 @@ const mongoose = require('mongoose');
 url =   process.env.NODE_ENV === 'test' ? TEST_DB : MONGO_URL;
 mongoose.connect(url,  { useNewUrlParser: true,
                              useUnifiedTopology: true })
-                            .then(() => console.log('Connexion à MongoDB réussie !'))
+                            .then(() => console.log('Connexion à MongoDB réussie  !'+ url))
                             .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
